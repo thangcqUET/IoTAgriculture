85d14861f56205a8fc452bbc256ff32bc97e98a1
thangcq <thangcq98@gmail.com>
2019-10-17 17:25:10 +0700

    rename class TestWeatherAPI & delete unnecessary class

--- Irrigation/src/TestWeatherAPI.java
+++ Irrigation/src/TestWeatherAPI.java
@@ -10,7 +10,7 @@ import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class TestAPI {
+public class TestWeatherAPI {
 
     public static void GetInfoWeather() {
         System.out.println("Weather .....");
